a9968330343916a8b6bdacf3fd0343d26ada7789
aeham <ayham.obtt@gmail.com>
2021-09-21 16:31:19 -0400

    initial commit

---
+++ week2lab2.cpp
@@ -0,0 +1,61 @@
+﻿// week2lab2.cpp : Defines the entry point for the application.
+//
+
+#include"iostream"
+
+using namespace std;
+
+namespace cst8219 {
+
+	class Vehicle {
+		// initlaizing two integer varibales by private defualt access
+
+	private:
+		int numWheels;
+		int numDoors;
+
+
+
+
+		// this first method/constractor talks no parameters 
+	public:
+		Vehicle() : Vehicle(4)
+		{
+			cout << "Constructor with 0 parameter" << endl;
+
+		}
+
+		// this second method/constractor talks one integer parameters 
+		Vehicle(int w) :Vehicle(w, 2)
+		{
+			cout << "Constructor with 1 parameter, where wheel" << w << endl;
+		}
+
+
+		// this third method/constractor talks two integer parameters 
+		Vehicle(int w, int d)
+		{
+			// and store them in w,d that present numWheels,numDoors
+			numWheels = w;
+			numDoors = d;
+			cout << "Constructor with 2 parameter" << endl;
+		}
+		//that is the deconstractore calling automatically fuction
+		~Vehicle()
+		{
+			cout << " In destructo" << endl;
+		}
+	};
+};
+
+// the main method
+int main(int argc, char** argv)
+{
+
+	cst8219::Vehicle myVehicle(4, 2);
+
+	//An object will be created by calling Vehicle(), two intermediate object, total three objects
+	cout << "I made a Vehicle" << endl;
+	return 0;
+
+}
